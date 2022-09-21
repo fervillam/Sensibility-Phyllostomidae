512a8ae219ec02ec469f0543307f4bf585c3734d
Fernando A. Villamizar-Moreno <fervillam07@gmail.com>
2022-09-21 10:25:29 -0500

    Update Script.R

--- Script.R
+++ Script.R
@@ -5,3 +5,18 @@
 # Setting work directory
 
 setwd("Documents/Phyllostomidae_Anc_Reconstruction")
+
+# Installing and loading required packages
+
+if(!require("pacman")){
+  install.packages("pacman")
+}
+
+library(pacman)
+
+pacman::p_load(ape, castor, dplyr, phangorn, phytools)
+
+# Pruned tree and codification matrices were taken from: #
+# https://github.com/DanielaP10/Sensibilidad-reconstruccion-de-estados-ancestrales #
+
+# Pruned tree is based on Upham et al. (2019).
